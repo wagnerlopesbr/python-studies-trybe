95379eeacac077c59f257d9fbf6e2a921f9888a3
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-04-30 15:53:21 -0300

    fixing renames

--- testing/testing_pytest_markers_code08.py
+++
@@ -1,7 +0,0 @@
-import time
-import pytest
-
-
-@pytest.mark.slow  # "slow" could be any name you want to use
-def test_slow_marker():
-    time.sleep(2)  # Simulate a slow test; in this case, 10 seconds
